e1a3a9bc9bb2a944e436a883db5bfb5e0d649a9d
Aaron Buitenwerf <aaron.buitenwerf@carleton.ca>
2020-05-15 18:06:23 -0400

    changed the camera controller and player hitboxes
    There is stuttering movement when the player moves.. could be a mix of camera and rigidbody set up

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,8 +3,8 @@
 public class CameraController : MonoBehaviour
 {
 
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    //public float smoothSpeed = 0.125f;
+    public Vector3 offset = new Vector3(0, 0, -1);
 
     private Transform target;
     private bool targetFound = false;
@@ -15,13 +15,19 @@ public class CameraController : MonoBehaviour
         targetFound = true;
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
         if (targetFound)
         {
-            Vector3 desiredPosition = target.transform.position + offset;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            transform.position = smoothedPosition;
+            //Vector3 desiredPosition = target.transform.position + offset;
+            //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            //transform.position = smoothedPosition;
+
+            // Always Update to Exactly Targets Position + Offset
+            transform.position = new Vector3(
+                target.transform.position.x + offset.x,
+                target.transform.position.y + offset.y,
+                target.transform.position.z + offset.z);
         }
 
     }
