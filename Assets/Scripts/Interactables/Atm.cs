fcccd3871fa5af8bdd5d297fdb9b3ebfdb7b19c8
Aaron Buitenwerf <aaron.buitenwerf@carleton.ca>
2020-06-03 17:13:53 -0400

    Fixed store assets and added ATM art

---
+++ Assets/Scripts/Interactables/Atm.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Atm : MonoBehaviour, IInteractable
+{
+    [SerializeField] private int addedStamina;
+    [SerializeField] private int price;
+    private IconBox display;
+    private TextMeshProUGUI priceText;
+
+    private void Start()
+    {
+        priceText = GetComponentInChildren<TextMeshProUGUI>();
+        display = GetComponentInChildren<IconBox>();
+
+        priceText.text = price.ToString();
+    }
+
+    public void Interact(GameObject player)
+    {
+        PlayerController playerController = player.GetComponent<PlayerController>();
+        
+        if (playerController.money - price >= 0 && playerController.stamina != playerController.attributes.maxStamina)
+        {
+            playerController.SubtractMoney(price); //charge the player for food
+            playerController.AddStamina(addedStamina);
+        }
+    }
+    public void OpenDisplay()
+    {
+        display.SetIcon(null);//open display
+    }
+
+    public void CloseDisplay()
+    {
+        display.Close();
+    }
+}
